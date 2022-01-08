d46893dbc6ad0d2de0957558cf1fa2045e6c5116
pydevnepal <rajukhatri054@gmail.com>
2022-01-08 20:29:20 +0545

    add dice game

--- assignments/pydevnepal/projects/dice_game/src/main.rs
+++ assignments/pydevnepal/projects/dice_game/src/main.rs
@@ -1,3 +1,73 @@
+use rand::Rng;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    println!("Please input your guess:");
+
+    let mut guess = String::new();
+    println!("{} number!", secret_number);
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+    let guess: u32 = match guess.trim().parse() {
+        Ok(num) => num,
+        Err(_) => 400,
+    };
+
+    println!(
+        "You've guessed : {}, but you still have other 3 options",
+        guess
+    );
+    println!("Select any one option");
+    println!("1. Number is greater than: {}", guess);
+    println!("2. Number is smaller than: {}", guess);
+    println!("3. Number is equal to: {}", guess);
+
+    let mut option = String::new();
+    io::stdin()
+        .read_line(&mut option)
+        .expect("Failed to read line");
+    let option: u32 = match option.trim().parse() {
+        Ok(num) => num,
+        Err(_) => 400,
+    };
+    loop {
+        match option {
+            1 => {
+                if guess < secret_number {
+                    println!("you guessed correct");
+                    break;
+                } else {
+                    println!("wrong guess!");
+                    break;
+                }
+            }
+            2 => {
+                if guess > secret_number {
+                    println!("you guessed correct");
+                    break;
+                } else {
+                    println!("wrong guess! 2");
+                    break;
+                }
+            }
+            3 => {
+                if guess == secret_number {
+                    println!("you guessed correct");
+                    break;
+                } else {
+                    println!("wrong guess!");
+                    break;
+                }
+            }
+            _ => {
+                println!("bad choice");
+                break;
+            }
+        }
+    }
 }
