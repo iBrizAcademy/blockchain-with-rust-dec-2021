357a93c8bdba5686229dea1f77ad8ead1029df3f
pydevnepal <rajukhatri054@gmail.com>
2022-01-08 19:03:54 +0545

    refactor main

--- assignments/pydevnepal/projects/crypto_ticker/src/main.rs
+++ assignments/pydevnepal/projects/crypto_ticker/src/main.rs
@@ -1,65 +1,24 @@
-
-use reqwest::header::HeaderMap;
-use serde::Deserialize;
-use dotenv::dotenv;
-use std::env;
+mod serializer;
+use serializer::*;
 use std::io;
 
-
-#[derive(Debug, Deserialize)]
-#[allow(dead_code)]
-struct USD{
-	price: f64,
-	last_updated:String
-}
-
-#[derive(Debug, Deserialize)]
-#[allow(dead_code, non_snake_case)]
-struct Quotes{
-	USD: USD
-}
-
-#[derive(Debug, Deserialize)]
-#[allow(dead_code)]
-struct Data{
-    id: i32,
-    symbol: String,
-    name: String,
-    amount: i32,
-    last_updated: String,
-    quote: Quotes
-}
-
-#[derive(Debug, Deserialize)]
-struct ResponseData{
-    data: Data,
-}
-
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    dotenv().ok();
-
-    let api_key = env::var("API_KEY")?;
-
-    println!("Please enter symbol of the cyrpto: ");
-    let mut sym = String::new();
-    io::stdin()
+fn main() {
+    println!("\n\t CRYPTO TICKER \n");
+    let symbol = String::from("btc");
+    get_bitcoin_price(&symbol).unwrap();
+    loop {
+        println!(
+            "If you want to get the price of another crypto coin then enter the symbol of the coin \
+        else type 'exit' to quit."
+        );
+        let mut sym = String::new();
+        io::stdin()
             .read_line(&mut sym)
             .expect("Failed to read line");
-    let url = format!("https://pro-api.coinmarketcap.com/v1/tools/price-conversion?symbol={}&amount=1", sym);
-
-    let mut headers= HeaderMap::new();
-    headers.insert("X-CMC_PRO_API_KEY", api_key.parse().unwrap());
-
-    let client=reqwest::Client::new();
-    let resp = client
-        .get(url)
-        .headers(headers)
-        .send()
-        .await?;
-
-    let resp_json = resp.json::<ResponseData>().await?;
-    println!("current price is {:#?}", resp_json.data.quote.USD.price);
-
-    Ok(())
+        match sym.trim().to_lowercase().as_str() {
+            "exit" => break,
+            _ => println!("\nRESULT"),
+        }
+        get_bitcoin_price(&sym).unwrap();
+    }
 }
