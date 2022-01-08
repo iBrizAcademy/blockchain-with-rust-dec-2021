7b34d57976444d0daf00a82df70db51d22346a3a
pydevnepal <rajukhatri054@gmail.com>
2022-01-08 20:22:34 +0545

    refactor quiz game

--- assignments/pydevnepal/projects/quiz_game/src/main.rs
+++ assignments/pydevnepal/projects/quiz_game/src/main.rs
@@ -1,11 +1,26 @@
+// Writing a simple Quiz game in RUST programming language
+
 use rand::Rng;
 use std::io;
 
 fn main() {
-    println!("Quiz Game");
+    println!("**************************************************************************");
+    println!("\t\t WELCOME TO QUIZ GAME");
+    println!("\tplease enter the number between 1-4 to select correct option");
+    println!("**************************************************************************\n");
 
     let mut score: u32 = 0;
     let mut question_index: usize = 0;
+    let mut choices = Vec::<usize>::new();
+
+    while choices.len() != 5 {
+        let num = rand::thread_rng().gen_range(0..5);
+        if choices.contains(&num) {
+            continue;
+        } else {
+            choices.push(num);
+        }
+    }
 
     let questions = vec![
         "Control unit of a digital computer is often called the",
@@ -15,30 +30,30 @@ fn main() {
         "Modern computers compared to earlier computers are",
     ];
     let answer_choices = vec![
-        vec!["1. ICS", "2. Clock", "3. Nerve centre", "4. All of these"],
+        vec!["ICS", "Clock", "Nerve centre", "All of these"],
         vec![
-            "1. Artificial intelligence",
-            "2. Extremely low cost",
-            "3. Very expensive",
-            "4. Versatility",
+            "Artificial intelligence",
+            "Extremely low cost",
+            "Very expensive",
+            "Versatility",
         ],
         vec![
-            "1. The task is distributed throughout the system",
-            "2. There are many computers and terminals",
-            "3. The task is executed by a number of processors",
-            "4. All of these",
+            "The task is distributed throughout the system",
+            "There are many computers and terminals",
+            "The task is executed by a number of processors",
+            "All of these",
         ],
         vec![
-            "1. Accuracy",
-            "2. Storage capability",
-            "3. High speed",
-            "4. Versatility",
+            "Accuracy",
+            "Storage capability",
+            "High speed",
+            "Versatility",
         ],
         vec![
-            "1. Faster and smaller",
-            "2. Larger and stronger",
-            "3. Less reliable",
-            "4. Faster and larger",
+            "Faster and smaller",
+            "Larger and stronger",
+            "Less reliable",
+            "Faster and larger",
         ],
     ];
     let answers = vec![
@@ -50,24 +65,30 @@ fn main() {
     ];
 
     loop {
-        let num = rand::thread_rng().gen_range(0..5);
-        println!("{}", num);
+        let num = choices[question_index];
         let mut answer: String = String::new();
         println!("{}", questions[num]);
         println!("{:?}", answer_choices[num]);
         io::stdin()
             .read_line(&mut answer)
             .expect("Please enter a correct value");
-        if answer.trim().to_lowercase() == answers[num].to_lowercase() {
-            println!("Well done, this is the correct answer\n");
+
+        let my_num: usize = answer
+            .trim()
+            .parse()
+            .expect("please give me correct string number!");
+
+        if answer_choices[num][my_num - 1].to_lowercase() == answers[num].to_lowercase() {
+            println!("CORRECT!!\n");
             score += 1;
         } else {
-            println!("Sorry, try again")
+            println!("WRONG!!")
         }
         question_index += 1;
         if question_index >= questions.len() {
             question_index = 0;
             answer = String::new();
+
             println!("Quiz finished, results: {}/{}", score, questions.len());
             println!("Would you like to quit?(yes or y)");
             io::stdin()
@@ -77,7 +98,6 @@ fn main() {
                 "yes" | "y" => break,
                 _ => println!("You choose to continue"),
             }
-
             score = 0;
         }
     }
