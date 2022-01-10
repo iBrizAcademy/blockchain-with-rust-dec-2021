32db373d403103a0e61cf96030d049469924dfb5
Sandip Rijal <sandurijal03@hotmail.com>
2022-01-10 14:43:51 +0545

    quiz files added and option select feature for quiz is implemented

--- assignments/sandurijal03/week_4/quiz_app/src/main.rs
+++ assignments/sandurijal03/week_4/quiz_app/src/main.rs
@@ -4,6 +4,7 @@ use std::fs::File;
 use std::io::stdin;
 
 #[derive(Debug, Deserialize)]
+#[allow(non_snake_case)]
 struct Question {
     question: String,
     optionA: String,
@@ -15,7 +16,8 @@ struct Question {
 
 fn show_header() {
     println!("");
-    println!("----------QUIZ GAME-----------");
+    println!("---------------- QUIZ GAME ------------------");
+    println!("----------Select The proper option-----------");
     println!("");
 }
 
@@ -27,34 +29,35 @@ fn main() {
     let questions: Vec<Question> = serde_json::from_reader(file).expect("error");
 
     loop {
-        let mut answer: String = String::new();
+        let mut guess: String = String::new();
         println!("{}", &questions[question_index].question);
         let a = &questions[question_index].optionA;
         let b = &questions[question_index].optionB;
         let c = &questions[question_index].optionC;
         let d = &questions[question_index].optionD;
-
-        let statement = format!("a. {}, b. {}, c. {}, d. {}", a, b, c, d);
+        let answer = &questions[question_index].answer;
+        let statement = format!("1. {}, 2. {}, 3. {}, 4. {}", a, b, c, d);
         println!("{}", statement);
         stdin()
-            .read_line(&mut answer)
+            .read_line(&mut guess)
             .expect("Please enter correct value");
-        if answer.trim().to_lowercase() == questions[question_index].optionA.to_lowercase() {
+
+        if guess.trim() == answer.to_string() {
             score += 1;
         } else {
-            println!("Sorry try again");
+            println!("Sorry wrong guess");
         }
         question_index += 1;
         if question_index >= questions.len() {
             question_index = 0;
-            answer = String::new();
+            guess = String::new();
             println!("Quiz finished, results: {}/{}", score, questions.len());
             println!("Would you like to quit? y or n");
             stdin()
-                .read_line(&mut answer)
+                .read_line(&mut guess)
                 .expect("Please enter the correct value.");
 
-            match answer.trim().to_ascii_lowercase().as_str() {
+            match guess.trim().to_ascii_lowercase().as_str() {
                 "y" | "bye" => break,
                 _ => println!("you choose to continue"),
             }
